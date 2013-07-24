eb7acd727017e9753082fad0cffffb16ffd365c6
Matthew White <me@kaishi.ro>
2013-07-24 15:04:00 -0400

    Removing block level code to templates folder for insertion aplenty

---
+++ templates/head.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="html no-js"> <!--<![endif]-->
+  
+<head>
+  
+  <!-- Meta -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+
+  <!-- Title -->
+  <title></title>
+
+  <!-- Favicon -->
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+  <!-- Style: Reset -->
+  <link rel="stylesheet" href="/css/reset.css">
+  
+  <!-- Style: Global -->
+  <link rel="stylesheet" href="/css/global.css">
+  
+  
+  <!-- Style: Fontello -->
+  <link rel="stylesheet" href="/css/fontello.css">
+  <link rel="stylesheet" href="/css/misolight.css">
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'> 
+
+  <!-- Style: Blocks -->
+  <link rel="stylesheet" href="/css/blocks/page.css">
+  <link rel="stylesheet" href="/css/blocks/header.css">
+  <link rel="stylesheet" href="/css/blocks/color-bar.css">
+  <link rel="stylesheet" href="/css/blocks/logo.css">
+  <link rel="stylesheet" href="/css/blocks/nav.css">
+  <link rel="stylesheet" href="/css/blocks/nav-toggle.css">
+  <link rel="stylesheet" href="/css/blocks/body.css">
+  <link rel="stylesheet" href="/css/blocks/hero.css">
+  <link rel="stylesheet" href="/css/blocks/profiles.css">
+  <link rel="stylesheet" href="/css/blocks/profile.css">
+  <link rel="stylesheet" href="/css/blocks/progress.css">
+  <link rel="stylesheet" href="/css/blocks/progress-bar.css">
+  <link rel="stylesheet" href="/css/blocks/press.css">
+  <link rel="stylesheet" href="/css/blocks/footer.css">
+  <link rel="stylesheet" href="/css/blocks/mailing-list.css">
+  <link rel="stylesheet" href="/css/blocks/copyright.css">
+  <link rel="stylesheet" href="/css/blocks/links.css">
+  <!-- Style: Grid -->
+  <link rel="stylesheet" href="/css/grid.css">
+
+  <!-- Script: Modernizr --> 
+  <script src="/js/vendor/html5shiv.js"></script>
+  <script src="/js/vendor/modernizr.custom-2.6.2-min.js"></script>
+
+</head>
+
+<body class="page">
