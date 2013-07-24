73cf69f9de864b7340513b76461da89b64dc6dba
Matthew White <me@kaishi.ro>
2013-07-24 15:59:14 -0400

    Adding server root var

--- bin/functions.php
+++ bin/functions.php
@@ -1,9 +1,10 @@
 <?php 
 
   function include_block($block,$n=1) {
+    $docroot = $_SERVER['DOCUMENT_ROOT'];
     for ($i=1; $i<=$n; $i++)
     {
-    include 'templates/blocks/'. $block .'.html';
+    include "$docroot/templates/blocks/". $block .".html";
     }
   }
 
